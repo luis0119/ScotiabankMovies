ba3f9c6996a232b7f0ef3b8d7fe3077e8e3cf6e6
luis0119 <88351381+luis0119@users.noreply.github.com>
2021-08-06 10:09:24 -0500

    Adjusted implementation.

--- app/src/main/java/com/example/movies/App.kt
+++ app/src/main/java/com/example/movies/App.kt
@@ -1,22 +1,7 @@
 package com.example.movies
 
 import android.app.Application
-import com.example.movies.di.AppComponent
-import com.example.movies.di.DaggerAppComponent
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-
-    val component: AppComponent by lazy {
-        DaggerAppComponent.builder().application(this).build()
-    }
-
-
-    override fun onCreate() {
-        super.onCreate()
-        component.inject(this)
-
-    }
-
-}
+class App : Application()
